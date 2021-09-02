caa4e82b2b1a5bf6e044b03640e5c04bae6c83af
“KazushiTakenaka” <take.kazushi@gmail.com>
2021-09-02 18:46:11 +0900

    test

---
+++ task1-2.java
@@ -0,0 +1,12 @@
+package task;
+
+public class task1-2 {
+
+	public static void main(String[] args) {
+		int w = 0;
+		for (int x = 0; x <= 100; x+=2) {
+			w = w + x;
+		}
+		System.out.println(w);
+	}
+}
