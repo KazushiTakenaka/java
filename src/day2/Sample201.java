6795cabb6d75a864bcb12311ff0a1db80a0911be
KazushiTakenaka <take.kazushi@gmail.com>
2021-09-09 18:13:38 +0900

    test_day2

---
+++ src/day2/Sample201.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class Sample201 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.print(5 + " + " + 2 + " = ");
+		System.out.println (5 + 2);
+		
+	}
+
+}
