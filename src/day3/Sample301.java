ed8f0c9e2b421c3c27e77c328e722bdd2cc09169
KazushiTakenaka <take.kazushi@gmail.com>
2021-09-10 21:36:06 +0900

    java

---
+++ src/day3/Sample301.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class Sample301 {
+
+	public static void main(String[] args) {
+		double a = 10;
+		System.out.println("a=" + a);
+		if(a > 0) {
+			System.out.println("aは生の数です。");
+		}
+		/*else {
+			System.out.println("負");
+		}*/
+		System.out.println(a < 0);
+		System.out.println(a > 0);
+	}
+
+}
