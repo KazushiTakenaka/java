a3bb7dd5a2bb392345c838a029f0e496218379ac
KazushiTakenaka <take.kazushi@gmail.com>
2021-09-11 14:09:21 +0900

    java401

---
+++ src/day4/Sample401.java
@@ -0,0 +1,12 @@
+package day4;
+
+public class Sample401 {
+
+	public static void main(String[] args) {
+		for(int i = 100; i >= 0; i-=2) {
+			System.out.println(i +" ");
+		}
+		//System.out.println();
+	}
+
+}
