80fe9f69b798aee55777ccced6f9c7519156e8c7
KazushiTakenaka <take.kazushi@gmail.com>
2021-09-12 23:05:52 +0900

    java406

---
+++ src/day4/Sample406.java
@@ -0,0 +1,19 @@
+package day4;
+
+public class Sample406 {
+
+	public static void main(String[] args) {
+		System.out.println("6が出たら終了");
+		//無限ループ
+		while(true) {
+			int dice = (int)(Math.random()*6)+1;
+			System.out.println(dice);
+			//System.out.print(" ");
+			if(dice == 6) {
+				break;       //ループから抜ける
+			}
+		}
+		System.out.println("終了");
+	}
+
+}
