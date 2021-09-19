10a031d2aba5a84f5306287067534ddaefc95d30
KazushiTakenaka <take.kazushi@gmail.com>
2021-09-19 14:08:11 +0900

    javaex40

---
+++ src/exday3/Chicken.java
@@ -0,0 +1,9 @@
+package exday3;
+
+public class Chicken extends Bird{
+	public Chicken() {
+		super("ニワトリ");
+	}
+	public void sing() {System.out.println("こけっこっこー");}
+
+}
--- src/exday3/SamleEx303.java
+++ src/exday3/SamleEx303.java
@@ -3,9 +3,10 @@ package exday3;
 public class SamleEx303 {
 
 	public static void main(String[] args) {
-		Bird b[] =  new Bird[2];		//Birdクラスの変数の配列を生成
+		Bird b[] =  new Bird[3];		//Birdクラスの変数の配列を生成
 		b[0] = new Crow2();				//b[0]にCrowクラスのインスタンスを生成
 		b[1] = new Sparrow2();			//b[1]にSparrowクラスのインスタンスを生成
+		b[2] = new Chicken();
 		for(int i = 0; i < b.length; i++) {
 			System.out.print(b[i].getName() + ":");
 			b[i].sing();
